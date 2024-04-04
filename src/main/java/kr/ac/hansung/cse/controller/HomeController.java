3b0b21a388618f51013c70e8a38300eb6d2267c0
jiyou228 <jhjy504@naver.com>
2024-04-04 17:31:40 +0900

    logout confirm

--- src/main/java/kr/ac/hansung/cse/controller/HomeController.java
+++ src/main/java/kr/ac/hansung/cse/controller/HomeController.java
@@ -18,10 +18,10 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(HttpServletRequest request) {
 
-        String url = request.getRequestURL().toString();
-        String clientIPaddr = request.getRemoteAddr();
-
-        logger.info("Request URL: {}, Client IP: {}", url, clientIPaddr);
+//        String url = request.getRequestURL().toString();
+//        String clientIPaddr = request.getRemoteAddr();
+//
+//        logger.info("Request URL: {}, Client IP: {}", url, clientIPaddr);
 
         return "home";
     }
